df6cff815504753c1f0db745358b886fdc500c56
ParteekSJ <parteeksjamwal@gmail.com>
2024-08-23 09:49:25 +0400

    0.04 Added LR Scheduling

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from config.default import get_cfg
 from models.pretrained_feat_extractor import get_pretrained_extractor
 from models.mmr import MMR
 from utils.loss import each_patch_loss_function
+from utils.optim_scheduler import mmr_lr_custom_scheduler
 from dataset.aebad_S import get_aebads
 from datetime import datetime
 from statistics import fmean
@@ -19,6 +20,13 @@ from statistics import fmean
 def freeze_params(backbone):
     for para in backbone.parameters():
         para.requires_grad = False
+
+
+def scratch_MAE_decoder(checkpoint):
+    for key_indv in list(checkpoint["model"].keys()):
+        if "decoder" in key_indv or key_indv == "mask_token":
+            checkpoint["model"].pop(key_indv)
+    return checkpoint
 
 
 if __name__ == "__main__":
@@ -32,24 +40,30 @@ if __name__ == "__main__":
     freeze_params(pretrained_feat_extractor)  # freezing pt model weights
 
     mmr_model = MMR(cfg=cfg)  # MAE + FPN
+    # Loading MAE-VIT-b Checkpoints
+    ckpt = torch.load(cfg.MODEL.mae_pt_ckpt)
+    # Remove all decoder-related parameters and the mask token values from the MAE-VIT ckpt
+    ckpt = scratch_MAE_decoder(ckpt)
+    mmr_model.mae.load_state_dict(ckpt["model"], strict=False)  # Load encoder weights
     mmr_model.to(device)
 
     # Load Dataset & Dataloaders
     dataset, dataloader = get_aebads(cfg)
 
-    # Create Optimizer
+    # Create Optimizer & Scheduler
     optimizer = torch.optim.AdamW(
         mmr_model.parameters(),
         lr=cfg.TRAIN_SETUPS.learning_rate,
         betas=(0.9, 0.95),
         weight_decay=cfg.TRAIN_SETUPS.weight_decay,
     )
+    scheduler = mmr_lr_custom_scheduler(optimizer, cfg)
 
     best_loss = 100.0
     total_steps = int(len(dataloader.dataset) / cfg.TRAIN_SETUPS.batch_size)
     running_loss = []
 
-    for epoch in range(10):
+    for epoch in range(cfg.TRAIN_SETUPS.epochs):
         pretrained_feat_extractor.eval()
         mmr_model.train()
 
@@ -78,6 +92,7 @@ if __name__ == "__main__":
                 )
 
         avg_epoch_loss = fmean(running_loss[-total_steps:])
+        scheduler.step()
 
         if avg_epoch_loss <= best_loss:
             # Save a checkpoint
@@ -85,3 +100,7 @@ if __name__ == "__main__":
             torch.save(checkpoint, f"{cfg.MODEL.save_ckpt}/MMR_{cur_time}.pth")
             print(f"MODEL SAVED with LOSS: {avg_epoch_loss}.")
             best_loss = avg_epoch_loss
+
+
+
+"""
--- utils/__init__.py
+++ utils/__init__.py
@@ -4,3 +4,4 @@ from .pos_embed import *
 from .random_masking import *
 from .patchify import *
 from .loss import *
+from .optim_scheduler import *
