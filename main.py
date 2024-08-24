eda4a829ab6e3b6b35b4c46be1b95fefab5f0597
ParteekSJ <parteeksjamwal@gmail.com>
2024-08-24 13:34:32 +0400

    0.09 Added Validation Testing & Checkpoint Saving

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ from dataset.aebad_S import get_aebads
 from datetime import datetime
 from statistics import fmean
 import os
+from inference import cal_anomaly_map
+from sklearn.metrics import roc_auc_score
+from utils.plot_predictions import plot_predictions
 
 
 def scratch_MAE_decoder(checkpoint):
@@ -40,18 +43,26 @@ if __name__ == "__main__":
     pretrained_feat_extractor = get_pretrained_extractor(return_nodes=cfg.MODEL.return_nodes)
     pretrained_feat_extractor.to(device)
     freeze_params(pretrained_feat_extractor)  # freezing pt model weights
+    print("Pretrained Model Loaded.")
 
     mmr_model = MMR(cfg=cfg)  # MAE + FPN
     # Loading MAE-VIT-b Checkpoints
     ckpt = torch.load(cfg.MODEL.mae_pt_ckpt)
     # Remove all decoder-related parameters and the mask token values from the MAE-VIT ckpt
     ckpt = scratch_MAE_decoder(ckpt)
-    mmr_model.mae.load_state_dict(ckpt["model"], strict=False)  # Load encoder weights
+    mmr_model.mae.load_state_dict(ckpt["model"], strict=False)  # Load encoder-only weights
     mmr_model.to(device)
+    print("MMR Model Loaded.")
+
+    p = torch.load(
+        f="/Users/parteeksj/Desktop/MMR_2024-08-22_19_30_07.pth", map_location="cpu"
+    )
+    mmr_model.load_state_dict(p["model"])
 
     # Load Dataset & Dataloaders
-    dataset, dataloader = get_aebads(cfg)
-    print(f"Total Samples: {len(dataset)}")
+    train_loader, test_loader = get_aebads(cfg)
+    print(f"Total Training Samples: {len(train_loader.dataset)}")
+    print(f"Total Testing Samples: {len(test_loader.dataset)}")
 
     # Check if 'checkpoints' folder is created. If not, create one.
     if not os.path.exists(cfg.MODEL.save_ckpt):
@@ -66,15 +77,20 @@ if __name__ == "__main__":
     )
     scheduler = mmr_lr_custom_scheduler(optimizer, cfg)
 
-    best_loss = 100.0
-    total_steps = int(len(dataloader.dataset) / cfg.TRAIN_SETUPS.batch_size)
-    running_loss = []
+    print("Optimizer & Scheduler Defined.")
 
+    best_loss = 100.0
+    best_auroc = 0.0
+    running_loss = []
+    total_train_steps = int(len(train_loader.dataset) / cfg.TRAIN_SETUPS.train_batch_size)
+    total_test_steps = int(len(test_loader.dataset) / cfg.TRAIN_SETUPS.test_batch_size)
+
+    # PERFORM TRAINING.
     for epoch in range(cfg.TRAIN_SETUPS.epochs):
         pretrained_feat_extractor.eval()
         mmr_model.train()
 
-        for idx, image in enumerate(dataloader):
+        for idx, image in enumerate(train_loader):
             image = image.to(device)
             with torch.no_grad():
                 pretrained_op_dict = pretrained_feat_extractor(image)
@@ -85,8 +101,10 @@ if __name__ == "__main__":
                 reverse_features[key] for key in cfg.MODEL.return_nodes
             ]
 
-            loss = each_patch_loss_function(multi_scale_features, multi_scale_reverse_features)
-            # print(f"E[{epoch}] - S[{idx}] - {loss.item()}")
+            loss = each_patch_loss_function(
+                multi_scale_features,
+                multi_scale_reverse_features,
+            )
 
             optimizer.zero_grad()
             loss.backward()
@@ -95,24 +113,82 @@ if __name__ == "__main__":
 
             if idx % cfg.MODEL.display_step == 0 and idx != 0:
                 print(
-                    f"[Epoch {epoch}, Step {idx}/{total_steps}]: {sum(running_loss[-cfg.MODEL.display_step:]) / cfg.MODEL.display_step}"
+                    f"[Epoch {epoch}, Step {idx}/{total_train_steps}]: {fmean(running_loss[-cfg.MODEL.display_step:])}"
                 )
 
-        avg_epoch_loss = fmean(running_loss[-total_steps:])
+        avg_epoch_loss = fmean(running_loss[-total_train_steps:])
         scheduler.step()
 
         if avg_epoch_loss <= best_loss:
             # Save a checkpoint
-            checkpoint = {"model": mmr_model.state_dict(), "epoch": epoch}
-            torch.save(checkpoint, f"{cfg.MODEL.save_ckpt}/MMR_{cur_time}.pth")
+            checkpoint = {
+                "model": mmr_model.state_dict(),
+                "epoch": epoch,
+                "loss": avg_epoch_loss,
+            }
+            torch.save(checkpoint, f"{cfg.MODEL.save_ckpt}/MMR_{cur_time}_LOSS.pth")
             print(f"[{epoch}] MODEL SAVED with LOSS: {avg_epoch_loss}.")
             best_loss = avg_epoch_loss
 
+        # PERFORM VALIDATION.
+        if (epoch + 1) % cfg.TRAIN_SETUPS.validation_every_epoch == 0:
+            mmr_model.eval()
 
-"""
+            auroc_arr = []
 
-Checkpoint Keys Dictionary
+            # Perform validation using test dataset here.
+            for idx, (image, mask) in enumerate(test_loader):
+                image, mask = image.to(device), mask.to(device)
 
-odict_keys(['cls_token', 'pos_embed', 'mask_token', 'decoder_pos_embed', 'patch_embed.proj.weight', 'patch_embed.proj.bias', 'blocks.0.norm1.weight', 'blocks.0.norm1.bias', 'blocks.0.attn.qkv.weight', 'blocks.0.attn.qkv.bias', 'blocks.0.attn.proj.weight', 'blocks.0.attn.proj.bias', 'blocks.0.norm2.weight', 'blocks.0.norm2.bias', 'blocks.0.mlp.fc1.weight', 'blocks.0.mlp.fc1.bias', 'blocks.0.mlp.fc2.weight', 'blocks.0.mlp.fc2.bias', 'blocks.1.norm1.weight', 'blocks.1.norm1.bias', 'blocks.1.attn.qkv.weight', 'blocks.1.attn.qkv.bias', 'blocks.1.attn.proj.weight', 'blocks.1.attn.proj.bias', 'blocks.1.norm2.weight', 'blocks.1.norm2.bias', 'blocks.1.mlp.fc1.weight', 'blocks.1.mlp.fc1.bias', 'blocks.1.mlp.fc2.weight', 'blocks.1.mlp.fc2.bias', 'blocks.2.norm1.weight', 'blocks.2.norm1.bias', 'blocks.2.attn.qkv.weight', 'blocks.2.attn.qkv.bias', 'blocks.2.attn.proj.weight', 'blocks.2.attn.proj.bias', 'blocks.2.norm2.weight', 'blocks.2.norm2.bias', 'blocks.2.mlp.fc1.weight', 'blocks.2.mlp.fc1.bias', 'blocks.2.mlp.fc2.weight', 'blocks.2.mlp.fc2.bias', 'blocks.3.norm1.weight', 'blocks.3.norm1.bias', 'blocks.3.attn.qkv.weight', 'blocks.3.attn.qkv.bias', 'blocks.3.attn.proj.weight', 'blocks.3.attn.proj.bias', 'blocks.3.norm2.weight', 'blocks.3.norm2.bias', 'blocks.3.mlp.fc1.weight', 'blocks.3.mlp.fc1.bias', 'blocks.3.mlp.fc2.weight', 'blocks.3.mlp.fc2.bias', 'blocks.4.norm1.weight', 'blocks.4.norm1.bias', 'blocks.4.attn.qkv.weight', 'blocks.4.attn.qkv.bias', 'blocks.4.attn.proj.weight', 'blocks.4.attn.proj.bias', 'blocks.4.norm2.weight', 'blocks.4.norm2.bias', 'blocks.4.mlp.fc1.weight', 'blocks.4.mlp.fc1.bias', 'blocks.4.mlp.fc2.weight', 'blocks.4.mlp.fc2.bias', 'blocks.5.norm1.weight', 'blocks.5.norm1.bias', 'blocks.5.attn.qkv.weight', 'blocks.5.attn.qkv.bias', 'blocks.5.attn.proj.weight', 'blocks.5.attn.proj.bias', 'blocks.5.norm2.weight', 'blocks.5.norm2.bias', 'blocks.5.mlp.fc1.weight', 'blocks.5.mlp.fc1.bias', 'blocks.5.mlp.fc2.weight', 'blocks.5.mlp.fc2.bias', 'blocks.6.norm1.weight', 'blocks.6.norm1.bias', 'blocks.6.attn.qkv.weight', 'blocks.6.attn.qkv.bias', 'blocks.6.attn.proj.weight', 'blocks.6.attn.proj.bias', 'blocks.6.norm2.weight', 'blocks.6.norm2.bias', 'blocks.6.mlp.fc1.weight', 'blocks.6.mlp.fc1.bias', 'blocks.6.mlp.fc2.weight', 'blocks.6.mlp.fc2.bias', 'blocks.7.norm1.weight', 'blocks.7.norm1.bias', 'blocks.7.attn.qkv.weight', 'blocks.7.attn.qkv.bias', 'blocks.7.attn.proj.weight', 'blocks.7.attn.proj.bias', 'blocks.7.norm2.weight', 'blocks.7.norm2.bias', 'blocks.7.mlp.fc1.weight', 'blocks.7.mlp.fc1.bias', 'blocks.7.mlp.fc2.weight', 'blocks.7.mlp.fc2.bias', 'blocks.8.norm1.weight', 'blocks.8.norm1.bias', 'blocks.8.attn.qkv.weight', 'blocks.8.attn.qkv.bias', 'blocks.8.attn.proj.weight', 'blocks.8.attn.proj.bias', 'blocks.8.norm2.weight', 'blocks.8.norm2.bias', 'blocks.8.mlp.fc1.weight', 'blocks.8.mlp.fc1.bias', 'blocks.8.mlp.fc2.weight', 'blocks.8.mlp.fc2.bias', 'blocks.9.norm1.weight', 'blocks.9.norm1.bias', 'blocks.9.attn.qkv.weight', 'blocks.9.attn.qkv.bias', 'blocks.9.attn.proj.weight', 'blocks.9.attn.proj.bias', 'blocks.9.norm2.weight', 'blocks.9.norm2.bias', 'blocks.9.mlp.fc1.weight', 'blocks.9.mlp.fc1.bias', 'blocks.9.mlp.fc2.weight', 'blocks.9.mlp.fc2.bias', 'blocks.10.norm1.weight', 'blocks.10.norm1.bias', 'blocks.10.attn.qkv.weight', 'blocks.10.attn.qkv.bias', 'blocks.10.attn.proj.weight', 'blocks.10.attn.proj.bias', 'blocks.10.norm2.weight', 'blocks.10.norm2.bias', 'blocks.10.mlp.fc1.weight', 'blocks.10.mlp.fc1.bias', 'blocks.10.mlp.fc2.weight', 'blocks.10.mlp.fc2.bias', 'blocks.11.norm1.weight', 'blocks.11.norm1.bias', 'blocks.11.attn.qkv.weight', 'blocks.11.attn.qkv.bias', 'blocks.11.attn.proj.weight', 'blocks.11.attn.proj.bias', 'blocks.11.norm2.weight', 'blocks.11.norm2.bias', 'blocks.11.mlp.fc1.weight', 'blocks.11.mlp.fc1.bias', 'blocks.11.mlp.fc2.weight', 'blocks.11.mlp.fc2.bias', 'norm.weight', 'norm.bias', 'decoder_embed.weight', 'decoder_embed.bias', 'decoder_blocks.0.norm1.weight', 'decoder_blocks.0.norm1.bias', 'decoder_blocks.0.attn.qkv.weight', 'decoder_blocks.0.attn.qkv.bias', 'decoder_blocks.0.attn.proj.weight', 'decoder_blocks.0.attn.proj.bias', 'decoder_blocks.0.norm2.weight', 'decoder_blocks.0.norm2.bias', 'decoder_blocks.0.mlp.fc1.weight', 'decoder_blocks.0.mlp.fc1.bias', 'decoder_blocks.0.mlp.fc2.weight', 'decoder_blocks.0.mlp.fc2.bias', 'decoder_blocks.1.norm1.weight', 'decoder_blocks.1.norm1.bias', 'decoder_blocks.1.attn.qkv.weight', 'decoder_blocks.1.attn.qkv.bias', 'decoder_blocks.1.attn.proj.weight', 'decoder_blocks.1.attn.proj.bias', 'decoder_blocks.1.norm2.weight', 'decoder_blocks.1.norm2.bias', 'decoder_blocks.1.mlp.fc1.weight', 'decoder_blocks.1.mlp.fc1.bias', 'decoder_blocks.1.mlp.fc2.weight', 'decoder_blocks.1.mlp.fc2.bias', 'decoder_blocks.2.norm1.weight', 'decoder_blocks.2.norm1.bias', 'decoder_blocks.2.attn.qkv.weight', 'decoder_blocks.2.attn.qkv.bias', 'decoder_blocks.2.attn.proj.weight', 'decoder_blocks.2.attn.proj.bias', 'decoder_blocks.2.norm2.weight', 'decoder_blocks.2.norm2.bias', 'decoder_blocks.2.mlp.fc1.weight', 'decoder_blocks.2.mlp.fc1.bias', 'decoder_blocks.2.mlp.fc2.weight', 'decoder_blocks.2.mlp.fc2.bias', 'decoder_blocks.3.norm1.weight', 'decoder_blocks.3.norm1.bias', 'decoder_blocks.3.attn.qkv.weight', 'decoder_blocks.3.attn.qkv.bias', 'decoder_blocks.3.attn.proj.weight', 'decoder_blocks.3.attn.proj.bias', 'decoder_blocks.3.norm2.weight', 'decoder_blocks.3.norm2.bias', 'decoder_blocks.3.mlp.fc1.weight', 'decoder_blocks.3.mlp.fc1.bias', 'decoder_blocks.3.mlp.fc2.weight', 'decoder_blocks.3.mlp.fc2.bias', 'decoder_blocks.4.norm1.weight', 'decoder_blocks.4.norm1.bias', 'decoder_blocks.4.attn.qkv.weight', 'decoder_blocks.4.attn.qkv.bias', 'decoder_blocks.4.attn.proj.weight', 'decoder_blocks.4.attn.proj.bias', 'decoder_blocks.4.norm2.weight', 'decoder_blocks.4.norm2.bias', 'decoder_blocks.4.mlp.fc1.weight', 'decoder_blocks.4.mlp.fc1.bias', 'decoder_blocks.4.mlp.fc2.weight', 'decoder_blocks.4.mlp.fc2.bias', 'decoder_blocks.5.norm1.weight', 'decoder_blocks.5.norm1.bias', 'decoder_blocks.5.attn.qkv.weight', 'decoder_blocks.5.attn.qkv.bias', 'decoder_blocks.5.attn.proj.weight', 'decoder_blocks.5.attn.proj.bias', 'decoder_blocks.5.norm2.weight', 'decoder_blocks.5.norm2.bias', 'decoder_blocks.5.mlp.fc1.weight', 'decoder_blocks.5.mlp.fc1.bias', 'decoder_blocks.5.mlp.fc2.weight', 'decoder_blocks.5.mlp.fc2.bias', 'decoder_blocks.6.norm1.weight', 'decoder_blocks.6.norm1.bias', 'decoder_blocks.6.attn.qkv.weight', 'decoder_blocks.6.attn.qkv.bias', 'decoder_blocks.6.attn.proj.weight', 'decoder_blocks.6.attn.proj.bias', 'decoder_blocks.6.norm2.weight', 'decoder_blocks.6.norm2.bias', 'decoder_blocks.6.mlp.fc1.weight', 'decoder_blocks.6.mlp.fc1.bias', 'decoder_blocks.6.mlp.fc2.weight', 'decoder_blocks.6.mlp.fc2.bias', 'decoder_blocks.7.norm1.weight', 'decoder_blocks.7.norm1.bias', 'decoder_blocks.7.attn.qkv.weight', 'decoder_blocks.7.attn.qkv.bias', 'decoder_blocks.7.attn.proj.weight', 'decoder_blocks.7.attn.proj.bias', 'decoder_blocks.7.norm2.weight', 'decoder_blocks.7.norm2.bias', 'decoder_blocks.7.mlp.fc1.weight', 'decoder_blocks.7.mlp.fc1.bias', 'decoder_blocks.7.mlp.fc2.weight', 'decoder_blocks.7.mlp.fc2.bias', 'decoder_norm.weight', 'decoder_norm.bias', 'decoder_pred.weight', 'decoder_pred.bias'])
+                with torch.no_grad():
+                    pretrained_op_dict = pretrained_feat_extractor(image)
 
-"""
+                multi_scale_features = [
+                    pretrained_op_dict[key] for key in cfg.MODEL.return_nodes
+                ]
+                reverse_features = mmr_model(image)
+                multi_scale_reverse_features = [
+                    reverse_features[key] for key in cfg.MODEL.return_nodes
+                ]
+
+                anomaly_map, amap_list = cal_anomaly_map(
+                    multi_scale_features, multi_scale_reverse_features
+                )
+                # Thresholding the Mask
+                mask[mask > 0.0] = 1.0
+
+                # Calculating the AUROC Score
+                auroc_score = roc_auc_score(
+                    mask.flatten(),
+                    torch.from_numpy(anomaly_map).flatten(),
+                )
+
+                auroc_arr.append(auroc_score)
+
+                if idx % cfg.MODEL.display_step == 0 and idx != 0:
+                    print(
+                        f"[Epoch {epoch}, Step {idx}/{total_test_steps}], AUROC: {fmean(auroc_arr[-cfg.MODEL.display_step:])}"
+                    )
+
+                # plot_predictions(
+                #     cfg=cfg,
+                #     test_image=image,
+                #     mask=mask,
+                #     anom_map=anomaly_map,
+                #     mode="2_2",
+                #     auroc_score=auroc_score,
+                #     save_path="./plotss",
+                #     image_name=str(idx),
+                # )
+
+        avg_auroc = fmean(auroc_arr)
+
+        if avg_auroc > best_auroc:
+            checkpoint = {
+                "model": mmr_model.state_dict(),
+                "epoch": epoch,
+                "auroc": avg_auroc,
+            }
+            torch.save(checkpoint, f"{cfg.MODEL.save_ckpt}/MMR_{cur_time}_AUROC.pth")
+            print(f"[{epoch}] MODEL SAVED with AUROC: {avg_auroc}.")
+            best_auroc = avg_auroc
